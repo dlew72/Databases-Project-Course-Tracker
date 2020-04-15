1d3abaf39e719199504ecd91eed8dc83f4b96071
dlew72 <51759034+dlew72@users.noreply.github.com>
2020-04-15 11:42:11 -0400

    Update course-tracker-data.sql

--- course-tracker-data.sql
+++ course-tracker-data.sql
@@ -2,7 +2,7 @@ INSERT INTO student (fname, minit, lname, stID, DOB, major, sex, minor)
 VALUES ('George', 'S', 'Long', 01918234, '04-FEB-00', 'CPE', 'M', 'Comp Sci');
 
 INSERT INTO student (fname, minit, lname, stID, DOB, major, sex, minor)
-VALUES ('Danny', 'S', 'Lewis', 0, '', 'CPE', 'M', 'Comp Sci');
+VALUES ('Danny', 'S', 'Lewis', 01696748, '23-MAY-00', 'CPE', 'M', 'Comp Sci');
 
 INSERT INTO student (fname, minit, lname, stID, DOB, major, sex, minor)
 VALUES ('Andrew', 'K', 'Donofrio', 01635876, '02-JUN-99', 'Math', 'M', 'Comp Sci');
