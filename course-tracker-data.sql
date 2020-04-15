af489d4d9e612215fc615207c3dfc92e17fc7629
glong00 <63614529+glong00@users.noreply.github.com>
2020-04-15 10:14:08 -0400

    Create course-tracker-data.sql

---
+++ course-tracker-data.sql
@@ -0,0 +1,9 @@
+INSERT INTO student (fname, minit, lname, stID, DOB, major, sex, minor)
+VALUES ('George', 'S', 'Long', 01918234, '04-FEB-00', 'CPE', 'M', 'Comp Sci');
+
+INSERT INTO student (fname, minit, lname, stID, DOB, major, sex, minor)
+VALUES ('Danny', 'S', 'Lewis', 0, '', 'CPE', 'M', 'Comp Sci');
+
+INSERT INTO student (fname, minit, lname, stID, DOB, major, sex, minor)
+VALUES ('Andrew', 'K', 'Donofrio', 01635876, '02-JUN-99', 'Math', 'M', 'Comp Sci');
+
