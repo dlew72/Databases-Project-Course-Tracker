e989e0f564cf31ac48131ce35a86137aab3ebdc7
ADono16 <63617148+ADono16@users.noreply.github.com>
2020-04-15 12:32:57 -0400

    Update data2.sql

--- data2.sql
+++ data2.sql
@@ -3,3 +3,45 @@ VALUES ('Herbert', 'C', 'Hoover', 34293433, '10-AUG-74', 'Mechanical Eng', 'M', 
 
 INSERT INTO student (fname, minit, lname, stID, DOB, major, sex, minor)
 VALUES ('Mae', 'C', 'Jemison', 01903023, '17-OCT-56', 'Chemical Eng', 'F', 'Africana Studies');
+
+INSERT INTO student (fname, minit, lname, stID, DOB, major, sex, minor)
+VALUES ('Hedy', 'E', 'Lamarr', 1114012000, '9-NOV-14', 'Electrical Eng', 'F', 'Theater');
+
+INSERT INTO student (fname, minit, lname, stID, DOB, major, sex, minor)
+VALUES ('Neil', 'A', 'Armstrong', 08141230, '5-AUG-30', 'Mechanical Eng', 'M', 'Aeronautical Eng');
+        
+INSERT INTO student (fname, minit, lname, stID, DOB, major, sex, minor)
+VALUES ('Alan', 'M', 'Turing', 01010101, '23-JUN-12', 'Math', 'M', 'Comp Sci');
+
+INSERT INTO student (fname, minit, lname, stID, DOB, major, sex, minor)
+VALUES ('Ada', 'A', 'Lovelace', 12151152, '10-DEC-15', 'Computer Eng', 'F', 'Comp Sci');
+
+INSERT INTO student (fname, minit, lname, stID, DOB, major, sex, minor)
+VALUES ('Tom', 'D', 'Scholz', 08251976, '10-MAR-47', 'Mechanical Eng', 'M', 'Music');
+
+INSERT INTO student (fname, minit, lname, stID, DOB, major, sex, minor)
+VALUES ('Leonardo', 'P', 'da Vinci', 04520519, '15-APR-52', 'Mechanical Eng', 'M', 'Art History');
+
+INSERT INTO student (fname, minit, lname, stID, DOB, major, sex, minor)
+VALUES ('Archimedes', 'O', 'Syracuse', 28721275, '1-JAN-87', 'Mechanical Eng', 'M', 'Math');
+
+INSERT INTO student (fname, minit, lname, stID, DOB, major, sex, minor)
+VALUES ('Frank', 'L', 'Wright', 68674959, '8-JUN-67', 'Civil Eng', 'M', 'Architecture');
+
+INSERT INTO student (fname, minit, lname, stID, DOB, major, sex, minor)
+VALUES ('Rowan', 'S', 'Atkinson', 06011955, '6-JAN-55', 'Electrical Eng', 'M', 'Theater');
+
+INSERT INTO student (fname, lname, stID, DOB, major, sex, minor)
+VALUES ('Dolph', 'Lundgren', 03111957, '3-NOV-57', 'Chemical Eng', 'M', 'Theater');
+
+INSERT INTO student (fname, lname, stID, DOB, major, sex, minor)
+VALUES ('Nikola', 'Tesla', 17567143, '10-JUL-56', 'Electrical Eng', 'M', 'Physics');
+
+INSERT INTO student (fname, minit, lname, stID, DOB, major, sex, minor)
+VALUES ('Grace', 'B', 'Hopper', 12961192, '9-DEC-06', 'Computer Eng', 'F', 'Math');
+
+INSERT INTO student (fname, minit, lname, stID, DOB, major, sex, minor)
+VALUES ('Ashton', 'C', 'Kutcher', 27197870, '7-FEB-78', 'Chemical Eng', 'M', 'Theater');
+
+INSERT INTO student (fname, lname, stID, DOB, major, sex)
+VALUES ('George', 'Stephenson', 96811435, '9-JUN-81', 'Civil Eng', 'M');
