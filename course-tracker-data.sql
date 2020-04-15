1d00aca0eebb9503bb0a3fbe42ed2f40456ab5ba
glong00 <63614529+glong00@users.noreply.github.com>
2020-04-15 10:30:52 -0400

    Update course-tracker-data.sql

--- course-tracker-data.sql
+++ course-tracker-data.sql
@@ -7,3 +7,20 @@ VALUES ('Danny', 'S', 'Lewis', 0, '', 'CPE', 'M', 'Comp Sci');
 INSERT INTO student (fname, minit, lname, stID, DOB, major, sex, minor)
 VALUES ('Andrew', 'K', 'Donofrio', 01635876, '02-JUN-99', 'Math', 'M', 'Comp Sci');
 
+INSERT INTO department (dHead, dHeadID, dName, dID, dPhone)
+VALUES ('Mobasseri', 123456789, 'Computer Eng', 000000001, 6105194958);
+
+INSERT INTO department (dHead, dHeadID, dName, dID, dPhone)
+VALUES ('Santhanam', 123456780, 'Mechanical Eng', 000000002, 6105194996);
+
+INSERT INTO department (dHead, dHeadID, dName, dID, dPhone)
+VALUES ('Comolli', 123456700, 'Chemical Eng', 000000003, 6105197134);
+
+INSERT INTO department (dHead, dHeadID, dName, dID, dPhone)
+VALUES ('Gross', 123456000, 'Civil Eng', 000000004, 6105195390);
+
+INSERT INTO department (dHead, dHeadID, dName, dID, dPhone)
+VALUES ('Mobasseri', 123456789, 'Electrical Eng', 000000005, 6105194958);
+
+
+
