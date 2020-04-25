1be3c7ad021ef4a06e9c822d52164dc04a338d34
glong00 <63614529+glong00@users.noreply.github.com>
2020-04-25 19:02:29 -0400

    Update course-tracker-data.sql

--- course-tracker-data.sql
+++ course-tracker-data.sql
@@ -2017,16 +2017,42 @@ INSERT INTO classes (stID, secNum, courseID, season)
 VALUES ( 26374859, '002', 'CEE- 2106' , 'Spring');
 
 INSERT INTO classes (stID, secNum, courseID, season)
-VALUES ( 68674959, '001', 'CEE- 1601' , 'Spring');
+VALUES ( 68674959, '001', 'CEE- 2106' , 'Spring');
 
 INSERT INTO classes (stID, secNum, courseID, season)
-VALUES ( 96811435, '002', 'CEE- 1601' , 'Spring');
+VALUES ( 96811435, '002', 'CEE- 2106' , 'Spring');
 
 INSERT INTO classes (stID, secNum, courseID, season)
-VALUES ( 17151783, '001', 'CEE- 1601' , 'Spring');
+VALUES ( 17151783, '001', 'CEE- 2106' , 'Spring');
 
 INSERT INTO classes (stID, secNum, courseID, season)
-VALUES ( 17923644, '002', 'CEE- 1601' , 'Spring');
+VALUES ( 17923644, '002', 'CEE- 2106' , 'Spring');
 
+INSERT INTO classes (stID, secNum, courseID, season)
+VALUES ( 37543217, '001', 'CEE- 3107' , 'Fall');
+
+INSERT INTO classes (stID, secNum, courseID, season)
+VALUES ( 59612011, '001', 'CEE- 3107' , 'Fall');
+
+INSERT INTO classes (stID, secNum, courseID, season)
+VALUES ( 90909005, '002', 'CEE- 3107' , 'Fall');
+
+INSERT INTO classes (stID, secNum, courseID, season)
+VALUES ( 84567210, '002', 'CEE- 3107' , 'Fall');
+
+INSERT INTO classes (stID, secNum, courseID, season)
+VALUES ( 26374859, '001', 'CEE- 3107' , 'Fall');
+
+INSERT INTO classes (stID, secNum, courseID, season)
+VALUES ( 68674959, '001', 'CEE- 3107' , 'Fall');
+
+INSERT INTO classes (stID, secNum, courseID, season)
+VALUES ( 96811435, '002', 'CEE- 3107' , 'Fall');
+
+INSERT INTO classes (stID, secNum, courseID, season)
+VALUES ( 17151783, '002', 'CEE- 3107' , 'Fall');
+
+INSERT INTO classes (stID, secNum, courseID, season)
+VALUES ( 17923644, '001', 'CEE- 3107' , 'Fall');
 
 
