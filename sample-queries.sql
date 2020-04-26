e343abf21932bc5e949528bfb188c30e837cc943
dlew72 <51759034+dlew72@users.noreply.github.com>
2020-04-26 15:28:18 -0400

    Update sample-queries.sql

--- sample-queries.sql
+++ sample-queries.sql
@@ -1,2 +1,20 @@
 --View all information about students
 SELECT * FROM STUDENT;
+
+--View all information about professors
+SELECT * FROM PROFESSOR;
+
+--View all information about departments
+SELECT * FROM DEPARTMENT;
+
+--View all information about courses
+SELECT * FROM COURSE;
+
+--View all information about semesters
+SELECT * FROM SEMESTER;
+
+--View all information about classes
+SELECT * FROM CLASSES;
+
+--View all information about sections
+SELECT * FROM SECTION;
